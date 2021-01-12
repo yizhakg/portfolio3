799875de5c67e978fef595b4c75e9a78924a92c7
yizhakg <yizhakg.gmail.com>
2021-01-12 16:46:33 +0200

    mediaquery fix

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function navToDiv(id) {
  let div = document.getElementById(id);
  div.scrollIntoView(false)
 }
+window.onload = ()=>{
+  let div = document.getElementById('header');
+  div.scrollIntoView(false)  
+}
