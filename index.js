9dd50ff8b714734960e2dedbb87097f994eef0a5
yizhakg <yizhakg.gmail.com>
2021-01-13 12:49:38 +0200

    background design

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function navToDiv(id) {
  let div = document.getElementById(id);
  div.scrollIntoView(false)
+ setTimeout(()=>{
+  let btn = document.getElementById('menu-btn');
+  btn.checked = false;
+ },500)
 }
 window.onload = ()=>{
   let div = document.getElementById('header');
