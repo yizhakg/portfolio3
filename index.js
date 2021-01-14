d8ad7c127adfcdf4e77270bc3c67a63e79d85b52
yizhakg <yizhakg.gmail.com>
2021-01-14 12:40:18 +0200

    change background settings

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 function navToDiv(id) {
- let div = document.getElementById(id);
- div.scrollIntoView(false)
- setTimeout(()=>{
-  let btn = document.getElementById('menu-btn');
-  btn.checked = false;
- },500)
+  let div = document.getElementById(id);
+  div.scrollIntoView(false)
+  setTimeout(() => {
+    let btn = document.getElementById('menu-btn');
+    btn.checked = false;
+  }, 500)
 }
-window.onload = ()=>{
+function addBackgroundText() {
+  let backgroundDiv = document.querySelector('.background');
+  for (i = 0; i < 120; i++) {
+    backgroundDiv.innerHTML += "Lorem ipsum dolor sit amet consectetur adipisicing elit. At magnam expedita repellendus repellat iure, harum aliquid soluta est. Dicta ut ullam distinctio aperiam repudiandae consequuntur esse. Quae, eaque totam. Est."
+  }
+}
+window.onload = () => {
   let div = document.getElementById('header');
-  div.scrollIntoView();  
+  div.scrollIntoView();
+  addBackgroundText();
 }
